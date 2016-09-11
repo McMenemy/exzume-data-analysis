48c7b4292c1ba4dca972734c67a2c063ac218112
McMenemy <mr_mcmenemy@yahoo.com>
2016-09-11 13:06:25 -0700

    add settings for dev vs prod

--- app.py
+++ app.py
@@ -6,8 +6,13 @@
 #   run $ flask run --host=0.0.0.0
 #   local port is http://0.0.0.0:5000/
 
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
