8ea365375c31d9e309d8a02a820efb2fc273642e
Alan Yan <acyanlight@gmail.com>
2016-09-11 19:02:40 -0700

    explore correlate button and routes generally work

--- app.py
+++ app.py
@@ -7,7 +7,9 @@
 #   local port is http://0.0.0.0:5000/
 
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import json
+from pandas import DataFrame
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -16,13 +18,15 @@ print(os.environ['APP_SETTINGS'])
 
 @app.route('/', methods=['POST','GET'])
 def correlate():
-    if request.headers['Content-Type'] == 'application/json':
-        print(request.data)
+    if request.method == 'POST':
+        data_dict = json.loads(request.data.decode('utf-8'))
         return request.data
+        # return jsonify(json.loads(request.data.decode('utf-8')))
     elif request.method == 'GET':
-        return request.data
+        return jsonify(alan='alan')
+
 if __name__ == '__main__':
-            app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
 
 
 # import sys
